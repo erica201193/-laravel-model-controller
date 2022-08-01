3bc905c2d531a2213e0280eef48b93cd6076b723
erica201193 <erica.mancini20@gmail.com>
2022-08-01 10:39:06 +0200

    Completed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,18 +4,20 @@
 
 @section ('page_content')
     <div class="container">
-        <h1 class="py-3 text-center fw-bold">Elenco dei film</h1>
+        <h1 class="py-3 text-center fw-bold">Movies</h1>
         <div class="row row-cols-3">
 
             @foreach ($movies as $movie)
                 <div class="col">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                        <h5 class="card-title">{{ $movie->title }}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Titolo originale: {{ $movie->original_title }}</h6>
-                        <p class="card-text">Nazione: {{ $movie->nationality }}</p>
-                        <p class="card-text">Data produzione: {{ $movie->date }}</p>
-                        <p class="card-text">Punteggio: {{ $movie->vote }}/10</p>
+                    <div class="p-2 my-card my-2">
+                        <div class="card-body p-3">
+                            <div class="mb-4">
+                                <h5 class="card-title">{{ $movie->title }}</h5>
+                                <h6 class="text-muted">Titolo originale: {{ $movie->original_title }}</h6>
+                            </div>
+                            <p class="card-text">Nazione: {{ $movie->nationality }}</p>
+                            <p class="card-text">Data produzione: {{ $movie->date }}</p>
+                            <p class="card-text">Punteggio: {{ $movie->vote }} / 10</p>
                         </div>
                     </div>
                 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body>
+<body class="my-bg">
     <main>
         @yield('page_content')
     </main>
